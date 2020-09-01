c37cb643d8c9eee2a7d5c02b9a7ba4ad5e6a3dbd
Iván Lovich <ivan.n.lovich@gmail.com>
2020-09-01 12:34:23 -0300

    backend folders url and filters added

--- skillTest/back/abm/todo/views.py
+++ skillTest/back/abm/todo/views.py
@@ -1,9 +1,17 @@
+from rest_framework import serializers
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from .serializers import TaskSerializer
-from .models import Task
+from .serializers import TaskSerializer, FolderSerializer
+from .models import Task, Folder
 
+
+class FolderViewSet(viewsets.ModelViewSet):
+    serializer_class = FolderSerializer
+    queryset = Folder.objects.all()
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filterset_fields = ('folder', )
